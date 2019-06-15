9a4a2a90bdf2066c52621c429dcdd5c355ba5900
danil <jokcik@yandex.ru>
2019-06-15 22:35:44 +0300

    fafsdf

--- docker/start.sh
+++ docker/start.sh
@@ -7,6 +7,8 @@ fi
 cp ./crontab /etc/crontab
 cp ./supervisord.conf /etc/supervisor/conf.d/supervisord.conf
 
+apt-get install supervisor -yqq
+
 # Prepend environemt variables to the crontab
 env |cat - /etc/crontab > /tmp/crontab
 mv /tmp/crontab /etc/crontab
