99d41caab22dd5bee5eb1c4a966088facc523695
danil <jokcik@yandex.ru>
2019-06-15 22:30:28 +0300

    fafsdf

--- docker/start.sh
+++ docker/start.sh
@@ -4,6 +4,9 @@ if [[ ! -d /home/jd-docker/node_modules/ ]]; then
     npm i
 fi
 
+cp ./crontab /etc/crontab
+cp ./supervisord.conf /etc/supervisor/conf.d/supervisord.conf
+
 # Prepend environemt variables to the crontab
 env |cat - /etc/crontab > /tmp/crontab
 mv /tmp/crontab /etc/crontab
