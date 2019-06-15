9627555312b71d003a8f57cd0683d1b6d25bfbcc
danil <jokcik@yandex.ru>
2019-06-16 00:31:40 +0300

    fafsdf

--- docker/start.sh
+++ docker/start.sh
@@ -4,15 +4,4 @@ if [[ ! -d /home/jd-docker/node_modules/ ]]; then
     npm i
 fi
 
-cp ./crontab /etc/crontab
-cp ./supervisord.conf /etc/supervisor/conf.d/supervisord.conf
-
-apt-get install supervisor -yqq
-
-# Prepend environemt variables to the crontab
-env |cat - /etc/crontab > /tmp/crontab
-mv /tmp/crontab /etc/crontab
-/usr/bin/supervisord
-
-
 while true; do sleep 1000; done
