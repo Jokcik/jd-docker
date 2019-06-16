78962e45f69caa0ba26e1edf234779f8afbfe40a
danil <jokcik@yandex.ru>
2019-06-16 11:25:16 +0300

    fafsdf

--- start_jd.sh
+++ start_jd.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "start script"
-/usr/bin/docker exec -it `/usr/bin/docker ps -q` ts-node index.ts --useTor
+/usr/bin/docker exec `/usr/bin/docker ps -q` ts-node index.ts --useTor
